4e881c9e5520ccd8130a492c33e2659fca51f90d
WXjzcccc <96734913+WXjzcccc@users.noreply.github.com>
2024-01-23 17:32:31 +0800

    增加异常处理
    增加异常处理

--- helper/DBHelper.py
+++ helper/DBHelper.py
@@ -9,8 +9,12 @@ class DBHelper:
         """
         @sql:       要执行的SQL语句
         """
-        self.cursor.execute(sql)
-        rows = self.cursor.fetchall()
+        try:
+            self.cursor.execute(sql)
+            rows = self.cursor.fetchall()
+        except:
+            print(f'数据库中存在不支持的字段，请比对查询语句{sql}')
+            return [()]
         return rows
     
     def close(self) -> None:
